896d89d7721e4b5d57ef79adac26f476273c7f49
luis.rojarcarrillo <luis.rojarcarrillo@ucr.ac.cr>
2021-07-30 02:30:08 -0600

    Agregando base de datos y dominio

--- src/Domain/Core/CoreEntities/GrupoInvestigacion.cs
+++ src/Domain/Core/CoreEntities/GrupoInvestigacion.cs
@@ -8,5 +8,18 @@ namespace Examen2.Domain.Core.CoreEntities
         public string Descripcion { get; set; }
         public DateTime FechaCreacion { get; set; }
         public string IdCoordinador { get; set; }
+
+        public GrupoInvestigacion(string id, string nombre, string descripcion, DateTime fechaCreacion, string idCoordinador) 
+        {
+            Id = id;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            FechaCreacion = fechaCreacion;
+            IdCoordinador = idCoordinador;
+        }
+
+        public GrupoInvestigacion() 
+        { 
+        }
     }
 }
---
+++ tests/UnitTests/Domain/Core/GrupoInvestigacionTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Examen2.Domain.Core.CoreEntities;
+using Xunit;
+
+namespace Lab2.UnitTests.Domain.Core
+{
+    public class GrupoInvestigacionTest
+    {
+        [Fact]
+        public void TryCreateGrupoInvestigacion()
+        {
+            var newCanton = new GrupoInvestigacion();
+            /*
+            Assert.Equal("Agua Caliente", newCanton.Name); //Get and test Set
+            Assert.Equal("Cartago", newCanton.Province_Name); //Get and test Set
+            */
+        }
+    }
+}
